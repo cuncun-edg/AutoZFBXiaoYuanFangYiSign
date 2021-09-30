272dacfd2044cd569fecd11cb1a780d14f219853
cuncun-edg <77426305+cuncun-edg@users.noreply.github.com>
2021-09-30 19:56:35 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -43,7 +43,7 @@ server_chan =0
 # SERVER酱sendkey，两侧的引号别丢了
 # 查看网址 sct.ftqq.com/sendkey
 # 免费版可每日发送五条推送
-sendkey = '你的key'
+sendkey = 'SCT80554TCRQrlNLDQU3FwxbU1oK1TiwQ'
 
 # ##################################程序开始#########################################
 import time
